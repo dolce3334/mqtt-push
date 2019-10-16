2f6c0eb87defd40bf0e00d461433b0386f9dc66c
chenhang <hang.chen@waykichainhk.com>
2019-10-16 15:19:58 +0800

    update version -cz

--- wicc-mqtt-push-message/src/main/kotlin/com/waykichain/mqtt/push/message/MqttMsgType.kt
+++ wicc-mqtt-push-message/src/main/kotlin/com/waykichain/mqtt/push/message/MqttMsgType.kt
@@ -4,25 +4,26 @@ package com.waykichain.mqtt.push.message
  * @ClassName: MqttMsgType
  * @Date: 2019/9/20 14:44
  */
-enum class MqttMsgType(val code: Int, val desp: String) {
+enum class MqttMsgType(val code: Int, val desp: String, val className: String) {
 
     /**玩家相关消息 type为 100-199*/
-    CUSTOMER(     100, "玩家相关消息"),
+    CUSTOMER(     100, "玩家相关消息", "com.waykichain.mqtt.push.message.customer.CustomerMsg"),
 
 
     /**账户相关消息 type为 200-299*/
-    ACCOUNT(                        200, "账户相关消息"),
-    ACCOUNT_RECHARGE_FINISHED(      201, "充值到账"),
+    ACCOUNT(                        200, "账户相关消息", "com.waykichain.mqtt.push.message.account.AccountMsg"),
+    ACCOUNT_RECHARGE_FINISHED(      201, "充值到账", "com.waykichain.mqtt.push.message.account.AccountMsg"),
 
 
     /**数据相关消息 type为 300-399*/
-    DATA(         300, "数据相关消息"),
+    DATA(         300, "数据相关消息", "com.waykichain.mqtt.push.message.data.DataMsg"),
 
 
     /**活动相关消息 type为 400-499*/
-    ACTIVITY(     400, "活动相关消息"),
+    ACTIVITY(     400, "活动相关消息", "com.waykichain.mqtt.push.message.activity.ActivityMsg"),
 
 
     /**官方消息 type为 500-599*/
-    OFFICIALS(    500, "官方消息");
+    OFFICIALS(    500, "官方消息", "com.waykichain.mqtt.push.message.officials.OfficialsMsg");
+
 }
